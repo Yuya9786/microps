01d05c3c9dc6081d0ac7c9417e2e4e5f2b56fdce
oh-yuya <happy.cccccc.lemon@gmail.com>
2021-03-04 18:35:46 +0900

    add protocol manager upper ip

--- ip.c
+++ ip.c
@@ -22,11 +22,18 @@ struct ip_hdr {
     uint8_t options[0];
 };
 
+struct ip_protocol {
+    struct ip_protocol *next;
+    uint8_t type;
+    void (*handler)(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface *iface);
+};
+
 const ip_addr_t IP_ADDR_ANY       = 0x00000000; /* 0.0.0.0 */
 const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
 
 /* NOTE: if you want to add/delete the entries after net_run(), you need to protect these lists with a mutex. */
 static struct ip_iface *ifaces;
+static struct ip_protocol *protocols;
 
 int
 ip_addr_pton(const char *p, ip_addr_t *n)
@@ -205,6 +212,17 @@ ip_input(const uint8_t *data, size_t len, struct net_device *dev)
     debugf("dev=%s, iface=%s, protocol=%u, total=%u",
         dev->name, ip_addr_ntop(iface->unicast, addr, sizeof(addr)), hdr->protocol, total);
     ip_dump(data, total);
+
+    struct ip_protocol *proto;
+
+    for (proto = protocols; proto; proto = proto->next) {
+        if (hdr->protocol == proto->type) {
+            proto->handler((uint8_t *)(hdr+1), total-hlen, hdr->src, hdr->dst, iface);
+            return;
+        }
+    }
+
+    /* unsupported protocol */
 }
 
 static int
@@ -298,6 +316,31 @@ ip_output(uint8_t protocol, const uint8_t *data, size_t len, ip_addr_t src, ip_a
         return -1;
     }
     return len;
+}
+
+/* NOTE: must not be call after net_run() */
+int
+ip_protocol_register(uint8_t type, void (*handler)(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface *iface))
+{
+    struct ip_protocol *proto;
+
+    for (proto = protocols; proto; proto = proto->next) {
+        if (type == proto->type) {
+            errorf("already registered, type=%d", type);
+            return -1;
+        }
+    }
+    proto = calloc(1, sizeof(*proto));
+    if (!proto) {
+        errorf("calloc() failure");
+        return -1;
+    }
+    proto->type = type;
+    proto->handler = handler;
+    proto->next = protocols;
+    protocols = proto;
+    infof("registered, type=%d", proto->type);
+    return 0;
 }
 
 int
