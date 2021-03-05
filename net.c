2d13d3a7fdb5710890f1128aba914808a6472ba8
oh-yuya <happy.cccccc.lemon@gmail.com>
2021-03-05 19:44:06 +0900

    add arp timer

--- net.c
+++ net.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <signal.h>
+#include <sys/time.h>
 
 #include "util.h"
 #include "net.h"
@@ -23,9 +24,17 @@ struct net_protocol_queue_entry {
     size_t len;
 };
 
+struct net_timer {
+    struct net_timer *next;
+    struct timeval interval;
+    struct timeval last;
+    void (*handler)(void);
+};
+
 /* NOTE: if you want to add/delete the entries after net_run(), you need to protect these lists with a mutex. */
 static struct net_device *devices;
 static struct net_protocol *protocols;
+static struct net_timer *timers;
 
 struct net_device *
 net_device_alloc(void)
@@ -202,6 +211,26 @@ net_protocol_register(uint16_t type, void (*handler)(const uint8_t *data, size_t
     return 0;
 }
 
+/* NOTE: must not be call after net_run() */
+int
+net_timer_register(struct timeval interval, void (*handler)(void))
+{
+    struct net_timer *timer;
+
+    timer = calloc(1, sizeof(*timer));
+    if (!timer) {
+        errorf("calloc() failure");
+        return -1;
+    }
+    timer->interval = interval;
+    gettimeofday(&timer->last, NULL);
+    timer->handler = handler;
+    timer->next = timers;
+    timers = timer;
+    infof("registered: interval={%d, %d}", interval.tv_sec, interval.tv_usec);
+    return 0;
+}
+
 #define NET_THREAD_SLEEP_TIME 1000 /* micro seconds */
 
 static pthread_t thread;
@@ -214,6 +243,8 @@ net_thread(void *arg)
     struct net_device *dev;
     struct net_protocol *proto;
     struct net_protocol_queue_entry *entry;
+    struct net_timer *timer;
+    struct timeval now, diff;
 
     while (!terminate) {
         count = 0;
@@ -237,6 +268,14 @@ net_thread(void *arg)
                 proto->handler((uint8_t *)(entry+1), entry->len, entry->dev);
                 free(entry);
                 count++;
+            }
+        }
+        for (timer = timers; timer; timer = timer->next) {
+            gettimeofday(&now, NULL);
+            timersub(&now, &timer->last, &diff);
+            if (timercmp(&timer->interval, &diff, <) != 0) { /* true (!0) or false (0) */
+                timer->handler();
+                gettimeofday(&timer->last, NULL);
             }
         }
         if (!count) {
